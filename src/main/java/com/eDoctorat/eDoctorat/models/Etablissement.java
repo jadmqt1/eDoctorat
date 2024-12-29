610fac498604a5dba14d67e0a92a63839565f571
jad <jadmouktassid2005@gmail.com>
2024-12-29 19:14:08 +0100

    salut

--- src/main/java/com/eDoctorat/eDoctorat/models/Etablissement.java
+++ src/main/java/com/eDoctorat/eDoctorat/models/Etablissement.java
@@ -1,11 +1,14 @@
 package com.eDoctorat.eDoctorat.models;
 
 import java.util.Date;
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import lombok.Data;
 
@@ -18,4 +21,6 @@ public class Etablissement {
     private Long idEtablissement;
     private String nomEtablissement;
    
+     @OneToMany(mappedBy = "etablissement")
+    private List<Laboratoire> laboratoires;
 }
--- src/main/java/com/eDoctorat/eDoctorat/models/Laboratoire.java
+++ src/main/java/com/eDoctorat/eDoctorat/models/Laboratoire.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import lombok.Data;
 
@@ -21,9 +22,7 @@ public class Laboratoire {
     private String initial;
     private String ced_id;
     private String directeur_id;
-    private String etablissement_id;
-
-
-
-
+    @ManyToOne
+    @JoinColumn(name = "etablissement_id", nullable = false)
+    private Etablissement etablissement;
 }
